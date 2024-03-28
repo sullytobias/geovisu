686433f15cd0251f59b2de7b3c45b6feb878f885
Sully <147931276+sullytobias@users.noreply.github.com>
2024-03-28 13:33:25 +0100

    Update in component

--- src/Map3D.jsx
+++ src/Map3D.jsx
@@ -1,59 +1,59 @@
-import React, { useState, useEffect } from "react";
 import { Canvas } from "react-three-fiber";
-
-import { Line, OrbitControls } from "@react-three/drei";
-
+import { OrbitControls, Line } from "@react-three/drei";
 import countriesGeojson from "./geo.json";
 
-const Map3D = () => {
-    const [countryLines, setCountryLines] = useState([]);
+const CountryComponent = ({ country }) => {
+    const isMulti = country.geometry.type === "MultiPolygon";
 
-    useEffect(() => {
-        const lines = countriesGeojson.features.map((feature) => {
-            if (feature.geometry.type === "MultiPolygon") {
-                const coordinates = feature.geometry.coordinates.map(
-                    (polygonCoords) =>
-                        polygonCoords[0].map(([lon, lat]) => [lon, lat, 0])
-                );
-                return coordinates;
-            } else {
-                return [
-                    feature.geometry.coordinates[0].map(([lon, lat]) => [
-                        lon,
-                        lat,
-                        0,
-                    ]),
-                ];
-            }
-        });
+    const lines = () => {
+        if (isMulti) {
+            const coordinates = country.geometry.coordinates.map(
+                (polygonCoords) =>
+                    polygonCoords[0].map(([lon, lat]) => [lon, lat, 0])
+            );
+            return coordinates;
+        } else {
+            return [
+                country.geometry.coordinates[0].map(([lon, lat]) => [
+                    lon,
+                    lat,
+                    0,
+                ]),
+            ];
+        }
+    };
 
-        const closedLines = lines.map((country) => {
-            return country.map((polygon) => {
-                if (
-                    polygon.length > 1 &&
-                    (polygon[0][0] !== polygon[polygon.length - 1][0] ||
-                        polygon[0][1] !== polygon[polygon.length - 1][1])
-                ) {
-                    polygon.push([polygon[0][0], polygon[0][1], 0]);
-                }
-                return polygon;
-            });
-        });
-
-        setCountryLines(closedLines.flat());
-    }, []);
+    const closedLines = lines().map((polygon) => {
+        if (
+            polygon.length > 1 &&
+            (polygon[0][0] !== polygon[polygon.length - 1][0] ||
+                polygon[0][1] !== polygon[polygon.length - 1][1])
+        ) {
+            polygon.push([polygon[0][0], polygon[0][1], 0]);
+        }
+        return polygon;
+    });
 
     return (
-        <Canvas>
-            <OrbitControls />
-
-            {countryLines.map((country, index) => (
+        <>
+            {closedLines.map((countryLine, index) => (
                 <Line
                     key={index}
-                    points={country}
+                    points={countryLine}
                     color="white"
                     lineWidth={1}
                 />
+            ))}
+        </>
+    );
+};
+
+const Map3D = () => {
+    return (
+        <Canvas>
+            <OrbitControls />
+            {countriesGeojson.features.map((country, index) => (
+                <CountryComponent key={index} country={country} />
             ))}
         </Canvas>
     );
