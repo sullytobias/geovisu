e2b3ff09446f0df5977263cb0dfe313b75d877e4
Sully <147931276+sullytobias@users.noreply.github.com>
2024-04-02 16:38:28 +0200

    Load Texture and rotation + orbit logic

--- src/components/Planet.jsx
+++ src/components/Planet.jsx
@@ -1,8 +1,51 @@
-const Planet = ({ color, radius, ...props }) => {
+import { useRef, useEffect, useState } from "react";
+import { useFrame } from "@react-three/fiber";
+import { TextureLoader } from "three";
+
+const Planet = ({
+    position,
+    color,
+    radius,
+    sideralOrbit,
+    name,
+    rotationPeriod,
+}) => {
+    const planetRef = useRef();
+    const [planetTexture, setPlanetTexture] = useState(null);
+
+    useEffect(() => {
+        const texturePath = `assets/${name.toLowerCase()}_texture.jpg`;
+
+        const textureLoader = new TextureLoader();
+        textureLoader.load(texturePath, setPlanetTexture);
+    }, [name]);
+
+    const initialAngle =
+        2 * Math.PI * ((Date.now() % sideralOrbit) / sideralOrbit);
+
+    useFrame(({ clock }) => {
+        const elapsedTime = clock.getElapsedTime();
+
+        if (planetRef.current && initialAngle) {
+            const rotationAngle =
+                ((elapsedTime * 2 * Math.PI) / sideralOrbit) * 5;
+            const rotation = initialAngle + rotationAngle * 5;
+
+            const newX = Math.cos(rotation) * position[0];
+            const newZ = Math.sin(rotation) * position[0];
+
+            planetRef.current.position.x = newX;
+            planetRef.current.position.z = newZ;
+
+            planetRef.current.rotation.y = rotationAngle * rotationPeriod;
+        } else
+            planetRef.current.rotation.y = (elapsedTime * 2 * Math.PI) / 7.25;
+    });
+
     return (
-        <mesh {...props}>
+        <mesh position={position} ref={planetRef}>
             <sphereGeometry args={[radius, 32, 32]} />
-            <meshBasicMaterial color={color} />
+            {planetTexture && <meshBasicMaterial map={planetTexture} />}
         </mesh>
     );
 };
--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -4,33 +4,42 @@ import { OrbitControls, Stars } from "@react-three/drei";
 import Planet from "./Planet";
 
 const SolarSystem = ({ planets }) => {
+    const randomColor = () => {
+        const randomHex = Math.floor(Math.random() * 16777215).toString(16);
+        return "#" + randomHex;
+    };
+
     return (
-        <Canvas camera={{ position: [0, 0, 1000] }}>
+        <Canvas camera={{ position: [0, 0, 250], fov: 100 }}>
             <OrbitControls />
             <ambientLight intensity={0.5} />
             <pointLight position={[10, 10, 10]} />
-            <Stars radius={400} />
+            <Stars radius={400} count={10000} />
             <Suspense fallback={null}>
-                {planets.map((planet, index) => {
-                    console.log(planet.id, planet.semimajorAxis);
-                    return (
-                        <Planet
-                            key={index}
-                            position={
-                                planet.englishName === "Sun"
-                                    ? [0, 0, 0]
-                                    : [
-                                          planet.semimajorAxis / 8000000 +
-                                              80 * (index + 1),
-                                          0,
-                                          0,
-                                      ]
-                            }
-                            color="red"
-                            radius={planet.meanRadius / 10000}
-                        />
-                    );
-                })}
+                {planets.map((planet, index) => (
+                    <Planet
+                        key={index}
+                        position={
+                            planet.englishName === "Sun"
+                                ? [0, 0, 0]
+                                : [
+                                      planet.semimajorAxis / 8000000 +
+                                          80 * (index + 1),
+                                      0,
+                                      0,
+                                  ]
+                        }
+                        color={
+                            planet.englishName === "Sun"
+                                ? "yellow"
+                                : randomColor()
+                        }
+                        radius={planet.meanRadius / 10000}
+                        sideralOrbit={planet.sideralOrbit}
+                        rotationPeriod={planet.sideralRotation}
+                        name={planet.englishName}
+                    />
+                ))}
             </Suspense>
         </Canvas>
     );
